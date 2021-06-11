c2861905678cc929c8a70f6d287a6f0666703993
Pedro1234-code <78425126+Pedro1234-code@users.noreply.github.com>
2021-06-11 12:29:39 -0300

    Update ub.sh

--- ub.sh
+++ ub.sh
@@ -6,9 +6,9 @@ mkdir ubuntu-fs
 
 cd ubuntu-fs
 
-wget http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.1-base-amd64.tar.gz
+wget http://cdimage.ubuntu.com/ubuntu-base/releases/21.04/release/ubuntu-base-21.04-base-riscv64.tar.gz
 
-FOCAL=ubuntu-base-20.04.1-base-amd64.tar.gz
+FOCAL=ubuntu-base-20.04.1-base-riscv64.tar.gz
 
 tar -xzf $FOCAL
 
@@ -28,8 +28,12 @@ echo mount -o bind /sys ~/ubuntu-fs/sys >> ~/startubuntu.sh
 
 echo mount -o bind /dev ~/ubuntu-fs/dev >> ~/startubuntu.sh
 
-echo mount --make-rslave ~/ubuntu-fs/sys
+echo mount --make-rslave ~/ubuntu-fs/sys >> ~/startubuntu.sh
 
-echo mount --make-rslave ~/ubuntu-fs/dev
+echo mount --make-rslave ~/ubuntu-fs/dev >> ~/startubuntu.sh
+
+echo chroot ~/ubuntu-fs >> ~/startubuntu.sh
+
+
 
 echo Finished. Start Ubuntu Chroot with sudo bash ub.sh
