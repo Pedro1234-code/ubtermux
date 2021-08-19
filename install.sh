9d76571fbe5e5d1f00850183b43cf696149b278b
Pedro1234-code <78425126+Pedro1234-code@users.noreply.github.com>
2021-08-19 15:54:35 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ ARCHITECTURE=$(dpkg --print-architecture)
 
 cd ubuntu-fs
 
-# Change the Ubuntu version here. Default is 20.04 Focal Fossa. This script does not support installation of Ubuntu versions launched before 2016 LTS. To install Ubuntu 16.04, put 16.04.6, not only 16.04.
+# Change the Ubuntu version here. Default is 20.04 Focal Fossa. This script does not support installation of Ubuntu versions launched before 2016 LTS. To install Ubuntu 16.04, put 16.04.6, not only 16.04. To install 18.04, put 18.04.5.
 ubuntu_version=20.04
 
 sudo wget http://cdimage.ubuntu.com/ubuntu-base/releases/$ubuntu_version/release/ubuntu-base-$ubuntu_version-base-$ARCHITECTURE.tar.gz
