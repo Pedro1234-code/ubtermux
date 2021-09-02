67037dc0c4f04cb84faca254999e2ed45dae2d9f
Pedro1234-code <78425126+Pedro1234-code@users.noreply.github.com>
2021-09-02 16:13:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 echo Creating Ubuntu chroot environment
 
+sudo apt install proot -y
+
+sudo dnf install proot
+
+sudo apk add proot
+
 cd /
 
 sudo mkdir ubuntu-fs
@@ -10,8 +16,8 @@ ARCHITECTURE=$(dpkg --print-architecture)
 
 cd ubuntu-fs
 
-# Change the Ubuntu version here. Default is 20.04 Focal Fossa. This script does not support installation of Ubuntu versions launched before 2016 LTS. To install Ubuntu 16.04, put 16.04.6, not only 16.04. To install 18.04, put 18.04.5.
-ubuntu_version=20.04
+# Change the Ubuntu version here. This script does not support installation of Ubuntu versions launched before 2016 LTS. To install Ubuntu 16.04, put 16.04.6, not only 16.04. To install 18.04, put 18.04.5.
+ubuntu_version=16.04.6
 
 sudo wget http://cdimage.ubuntu.com/ubuntu-base/releases/$ubuntu_version/release/ubuntu-base-$ubuntu_version-base-$ARCHITECTURE.tar.gz
 
@@ -45,12 +51,12 @@ sudo chmod +x /usr/bin/startubuntu
 
 echo Installing things inside Ubuntu Chroot
 
-sudo chroot /ubuntu-fs/ apt update
+proot -r /ubuntu-fs -0 -w / -b /dev -b /proc -b /sys apt update
 
-sudo chroot /ubuntu-fs/ apt update
+proot -r /ubuntu-fs -0 -w / -b /dev -b /proc -b /sys apt update
 
-sudo chroot /ubuntu-fs/ apt upgrade -y
+proot -r /ubuntu-fs -0 -w / -b /dev -b /proc -b /sys apt upgrade -y
 
-sudo chroot /ubuntu-fs/ apt install nano vim sudo python wget ubuntu-release-upgrader-core -y
+proot -r /ubuntu-fs -0 -w / -b /dev -b /proc -b /sys apt install nano vim sudo python wget ubuntu-release-upgrader-core -y
 
 echo Finished. Start Ubuntu Chroot with sudo startubuntu
