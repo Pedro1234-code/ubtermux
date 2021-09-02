31c21882a7b490c8dd8fd98a0a9676f783131cdc
Pedro1234-code <78425126+Pedro1234-code@users.noreply.github.com>
2021-09-02 16:08:35 -0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-sudo mount -t proc /proc /ubuntu-fs/proc
+# Uncoment these lines to use Ubuntu with chroot, not PRoot
 
-sudo mount -o bind /sys /ubuntu-fs/sys
+# sudo mount -t proc /proc /ubuntu-fs/proc
 
-sudo mount -o bind /dev /ubuntu-fs/dev
+# sudo mount -o bind /sys /ubuntu-fs/sys
 
-sudo chroot /ubuntu-fs
+# sudo mount -o bind /dev /ubuntu-fs/dev
+
+# sudo chroot /ubuntu-fs
+
+proot -r /ubuntu-fs -0 -w / -b /dev -b /proc -b /sys /bin/sh
