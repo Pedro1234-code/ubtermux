4c8a3e0b870a8e684f8dc9e0583417bcb4316b43
Pedro1234-code <78425126+Pedro1234-code@users.noreply.github.com>
2021-09-08 12:50:54 -0300

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo Removing Ubuntu 21.04 chroot
+echo Removing Ubuntu 20.04 chroot
 
 sudo umount /ubuntu-fs/dev
 
@@ -9,5 +9,9 @@ sudo umount /ubuntu-fs/sys
 sudo umount /ubuntu-fs/proc
 
 sudo rm -rf /ubuntu-fs
+
+sudo rm /launch.sh
+
+sudo rm /usr/bin/startubuntu
 
 echo Finished
